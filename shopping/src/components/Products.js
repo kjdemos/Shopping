d57bf7a63b9b0736a857ad2408a6c96b8a079976
SenpaiYG <yaxye_yssss@hotmail.com>
2022-11-14 11:47:38 +0000

    Changes

--- shopping/src/components/Products.js
+++ shopping/src/components/Products.js
@@ -1,12 +1,44 @@
 import React, {useState, useEffect} from "react";
 import '../App.css'
-import axios from axios 
+import axios from 'axios'
+
 function Products() {
 
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState ([]);
+
+useEffect (() => {
+    setLoading(true)
+    axios ({
+        method:"GET",
+        url: "https://fakestoreapi.com/products"
+    })
+    .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+    })
+    .catch((e) => console.log(e))
+    .finally(() => setLoading(false))
+}, [])
 
     return (
         <div className="products-container">
-            Products
+            {loading && (
+                <div>
+                    {" "}
+                    <h1>Loading...</h1>
+                </div>
+            )}
+            {data.map((product) => (
+                <div key={product.id} className= "card">
+                <div> <img src={product.image} alt="#"/> </div>
+                <div className="card-description">
+                    <h6>{product.title}</h6>
+                    <h6>{`Price: £${product.price}`}</h6>
+                    <h6>{`Description: ${product.description}`}</h6>
+                </div>
+                </div>
+            ))}
         </div>
     );
 }
