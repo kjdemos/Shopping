ebb19de619aa756ddf2be21658aec6baf8313bd6
Wambo-9 <irenewambui1996@gmail.com>
2022-11-21 23:01:56 +0000

    added list and js code to searchbar.js and index.html

--- shopping/src/components/Searchbar.js
+++ shopping/src/components/Searchbar.js
@@ -1,4 +1,22 @@
+
+document.querySelector('#search').addEventListener('input', Searchbar)
+
 function Searchbar() {
+    const searchProduct= document.querySelector('#search'); 
+    const filter = searchProduct.value.toLowerCase(); 
+    const productItems = document.querySelectorAll('.card-description card')
+
+    productItems.forEach((item) => {
+        let text = item.textContent
+        if(text.toLowerCase().includes(filter.toLowerCase())){
+            item.style.display = ' '
+        }
+        else{
+            item.style.display ='Error'
+        }
+    });
+
+
 
     return (
         <div class="input-group mb-3 searchbar">
